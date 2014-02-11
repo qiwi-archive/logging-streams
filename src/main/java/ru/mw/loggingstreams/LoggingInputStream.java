ca660ee36506e284e4e78af3412ae82010081af5
Ilya Linnik <i.linnik@qiwi.ru>
2014-02-11 10:42:24 +0400

    Fixed bugs in synchronous logging

--- src/main/java/ru/mw/loggingstreams/LoggingInputStream.java
+++ src/main/java/ru/mw/loggingstreams/LoggingInputStream.java
@@ -2,7 +2,8 @@ package ru.mw.loggingstreams;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
 
 /**
  * Created by nixan on 11.12.13.
@@ -11,7 +12,9 @@ public class LoggingInputStream extends InputStream {
 
     private final InputStream mInputStream;
 
-    private final OutputStream mLoggerStream;
+    private final PrintStream mLoggerStream;
+
+    private final StringWriter mStringWriter = new StringWriter();
 
     /**
      * Class that provides on-fly logging for the data that goes through the InputStream
@@ -19,7 +22,7 @@ public class LoggingInputStream extends InputStream {
      * @param inputStream  wrapped stream
      * @param loggerStream stream for logging
      */
-    public LoggingInputStream(InputStream inputStream, OutputStream loggerStream) {
+    public LoggingInputStream(InputStream inputStream, PrintStream loggerStream) {
         mInputStream = inputStream;
         mLoggerStream = loggerStream;
     }
@@ -28,16 +31,17 @@ public class LoggingInputStream extends InputStream {
     public int read() throws IOException {
         int result = mInputStream.read();
         if (result > 0) {
-            mLoggerStream.write(result);
+            mStringWriter.write(result);
         }
         return result;
     }
+
 
     @Override
     public int read(byte[] bytes) throws IOException {
         int result = mInputStream.read(bytes);
         if (result > 0) {
-            mLoggerStream.write(bytes, 0, result);
+            mStringWriter.write(new String(bytes), 0, result);
         }
         return result;
     }
@@ -46,7 +50,7 @@ public class LoggingInputStream extends InputStream {
     public int read(byte[] bytes, int i, int i2) throws IOException {
         int result = mInputStream.read(bytes, i, i2);
         if (result > 0) {
-            mLoggerStream.write(bytes, i, result);
+            mStringWriter.write(new String(bytes), i, result);
         }
         return result;
     }
@@ -62,7 +66,7 @@ public class LoggingInputStream extends InputStream {
     @Override
     public void close() throws IOException {
         mInputStream.close();
-        mLoggerStream.write("\n".getBytes());
+        mLoggerStream.println(mStringWriter.getBuffer().toString());
     }
 
     @Override
@@ -83,5 +87,10 @@ public class LoggingInputStream extends InputStream {
     @Override
     public int available() throws IOException {
         return mInputStream.available();
+    }
+
+    public static interface LogAvailableCallback {
+
+        public void onLogIsAvailable(String log);
     }
 }
